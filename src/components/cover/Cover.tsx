8be5f1eb8581ec25c2436c3b098919ab5851c0df
hexaquarks <seseriusca@gmail.com>
2023-03-04 16:23:11 -0500

    Add more styling

--- src/components/cover/Cover.tsx
+++ src/components/cover/Cover.tsx
@@ -5,8 +5,16 @@ import { Parallax } from 'react-parallax';
 
 const Cover = () => {
     return (
-        <Parallax className='image' bgImage={Placeholder} strength={800}>
-            <div />
+        <Parallax className = 'image' bgImage={Placeholder} strength={800}>
+            <div className = 'parallax-grid'>
+                <div className='parallax-grid-description'>
+                    <h1 className='title'>Ecaterina Bujac</h1>
+                    <h3 className='subtitle'>Travailleuse Sociale</h3>
+                </div>
+                <div className='parallax-grid-image'>
+                    <img src={Placeholder} />
+                </div>
+            </div>
         </Parallax>
     )
 }
---
+++ src/components/description/Description.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Placeholder from '../../assets/image-placeholder.jpg'
+
+import { Parallax } from 'react-parallax';
+
+const Description = () => {
+    return (
+        <div className='description' />
+    )
+}
+
+export default Description
