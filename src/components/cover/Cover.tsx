867ba4f2a0db7c8ca2c89cd03ebf10b20e0401a4
hexaquarks <seseriusca@gmail.com>
2023-03-05 20:03:48 -0500

    Started using Material UI instead of raw css

--- src/components/cover/Cover.tsx
+++ src/components/cover/Cover.tsx
@@ -1,20 +1,50 @@
 import React from 'react'
 import Placeholder from '../../assets/image-placeholder.jpg'
+import { makeStyles } from 'tss-react/mui';
+import { Background, Parallax } from 'react-parallax';
+import { Grid, Typography } from '@mui/material';
 
-import { Parallax } from 'react-parallax';
+const useStyles = makeStyles()((theme) => {
+    return {
+      root: {
+        color: theme.palette.primary.main,
+        height: "100vh"
+      },
+      grid: {
+        paddingTop: "5%"
+      },
+      description: {
+        backgroundColor: "gray"
+      },
+      title: {
+
+      },
+      subtitle: {
+
+      },
+      image: {
+        backgroundColor: "gray"
+      },
+    };
+  });
 
 const Cover = () => {
+    const { classes } = useStyles();
     return (
-        <Parallax className = 'image' bgImage={Placeholder} strength={800}>
-            <div className = 'parallax-grid'>
-                <div className='parallax-grid-description'>
-                    <h1 className='title'>Ecaterina Bujac</h1>
-                    <h3 className='subtitle'>Travailleuse Sociale</h3>
-                </div>
-                <div className='parallax-grid-image'>
-                    <img src={Placeholder} />
-                </div>
-            </div>
+        <Parallax className={classes.root} bgImage={Placeholder} strength={800}>
+            <Grid container className={classes.grid}>
+                <Grid item sm={8} className={classes.description}>
+                    <Typography variant="h1" className={classes.title}>
+                        Ecaterina Bujac
+                    </Typography>
+                    <Typography variant="h3" className={classes.subtitle}>
+                        Travailleuse Sociale
+                    </Typography>
+                </Grid>
+                <Grid item sm={4} className={classes.image}>
+                    <img src={Placeholder}/>
+                </Grid>
+            </Grid>
         </Parallax>
     )
 }
